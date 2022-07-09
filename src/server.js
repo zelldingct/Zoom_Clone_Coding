6699991f4108ec9779c61816f094dcd525981a24
zelldingct <zell9931@gmail.com>
2022-07-10 02:17:10 +0900

    220710 1.6Recap

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -7,6 +9,20 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req,res) => res.redirect("/"));
+
 const handleListen = () => console.log('Listening on http://localhost:3000');
 
-app.listen(3000, handleListen);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server});
+
+wss.on("connection", (socket) => {
+ console.log("Connected to Browser");
+ socket.on("close", () => console.log("Disconnected from the Browser X"))
+ socket.on("message", (message) => {
+ const translateData = message.toString('utf8');
+ console.log(translateData);
+ });
+ socket.send("Hello!");
+});
+
+server.listen(3000, handleListen);
